dc625dc88d1ec8fdfa5724dfd4388e353fccd07e
f4br1c10 <fabriciopstos@outlook.com>
2022-07-20 21:55:00 -0300

    igniteLab_Aula_03

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import '../src/styles/global.css'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './lib/apollo'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 )
